b6576ad9764b277af0279d5f154bdbbf05f886dd
George Tzavelas <george.tzavelas@babylist.com>
2026-05-08 19:36:30 -0400

    chore: bump JS_VERSION to v210 for cache-bust

--- theoracleofdelphigzed.js
+++ theoracleofdelphigzed.js
@@ -18,12 +18,12 @@ define([
     "dojo","dojo/_base/declare",
     "ebg/core/gamegui",
     "ebg/counter",
-    g_gamethemeurl + "modules/js/HexGrid.js?v209",
-    g_gamethemeurl + "modules/js/Components.js?v209",
-    g_gamethemeurl + "modules/js/ClusterDefinitions.js?v209",
-    g_gamethemeurl + "modules/js/BoardBuilder.js?v209",
-    g_gamethemeurl + "modules/js/BoardRenderer.js?v209",
-    g_gamethemeurl + "modules/BX/js/DragScroller.js?v209",
+    g_gamethemeurl + "modules/js/HexGrid.js?v210",
+    g_gamethemeurl + "modules/js/Components.js?v210",
+    g_gamethemeurl + "modules/js/ClusterDefinitions.js?v210",
+    g_gamethemeurl + "modules/js/BoardBuilder.js?v210",
+    g_gamethemeurl + "modules/js/BoardRenderer.js?v210",
+    g_gamethemeurl + "modules/BX/js/DragScroller.js?v210",
 ],
 function (dojo, declare, gamegui, counter, HexGrid, Components, ClusterDefinitions, BoardBuilder, BoardRenderer) {
 
@@ -60,8 +60,8 @@ function (dojo, declare, gamegui, counter, HexGrid, Components, ClusterDefinitio
     return declare("bgagame.theoracleofdelphigzed", ebg.core.gamegui, {
 
         // Cache-bust version read by Components when loading dice libs.
-        // Keep in sync with the ?v209 markers in the define() block above.
-        JS_VERSION: "v209",
+        // Keep in sync with the ?v210 markers in the define() block above.
+        JS_VERSION: "v210",
 
         // Game components
         hexGrid: null,
